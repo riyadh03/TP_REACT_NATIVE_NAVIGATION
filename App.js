1ec51cb95dea20af3d4629bb854d8e32c38c67b2
riyadh03 <leprofessionnel3.0@gmail.com>
2025-12-11 16:09:46 +0100

    Ajout de l'authentification et des diffenretes otions de navigation

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import AuthProvider, { AuthContext } from "./context/AuthContext";
 
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from "./screens/DetailsScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import AppBar from "./screens/AppBar";
-
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
@@ -39,33 +39,37 @@ function HomeStack() {
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.safeArea}>
-        <NavigationContainer>
-          <AppBar />
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              headerShown: false,
-              tabBarActiveTintColor: "blue",
-              tabBarInactiveTintColor: "gray",
-              tabBarStyle: { backgroundColor: "#f0f0f0" },
-              tabBarLabelStyle: { fontSize: 14 },
-              tabBarIcon: ({ color, size }) => {
-                if (route.name === "Maison") {
-                  return <Ionicons name="home" size={size} color={color} />;
-                } else if (route.name === "Paramètres") {
-                  return <Ionicons name="settings" size={size} color={color} />;
-                }
-                return null;
-              },
-            })}
-          >
-            <Tab.Screen name="Maison" component={HomeStack} />
-            <Tab.Screen name="Paramètres" component={SettingsScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.safeArea}>
+          <NavigationContainer>
+            <AppBar />
+            <Tab.Navigator
+              screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarActiveTintColor: "blue",
+                tabBarInactiveTintColor: "gray",
+                tabBarStyle: { backgroundColor: "#f0f0f0" },
+                tabBarLabelStyle: { fontSize: 14 },
+                tabBarIcon: ({ color, size }) => {
+                  if (route.name === "Maison") {
+                    return <Ionicons name="home" size={size} color={color} />;
+                  } else if (route.name === "Paramètres") {
+                    return (
+                      <Ionicons name="settings" size={size} color={color} />
+                    );
+                  }
+                  return null;
+                },
+              })}
+            >
+              <Tab.Screen name="Maison" component={HomeStack} />
+              <Tab.Screen name="Paramètres" component={SettingsScreen} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 }
 
